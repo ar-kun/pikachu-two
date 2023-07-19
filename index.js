c5f5bb90d343e41d50639dc39c8b67c7b607f08f
Adam Ramdan <arshinks12@gmail.com>
2023-07-20 00:30:38 +0700

    ft: first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+async function getPikachu() {
+ const result = [];
+
+ try {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=36');
+  const data = await response.json();
+
+  for (const pokemon of data.results) {
+   const pokemonResponse = await fetch(pokemon.url);
+   const pokemonData = await pokemonResponse.json();
+   result.push(pokemonData);
+  }
+  displayResults(result);
+ } catch (error) {
+  console.error('Error fetching data:', error);
+ }
+}
+
+function displayResults(data) {
+ const content = document.querySelector('.root');
+ data.forEach((pokemon) => {
+  content.innerHTML += `
+  <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+ <div class="flex justify-end px-4 pt-4">
+  <button
+   id="dropdownButton"
+   data-dropdown-toggle="dropdown"
+   class="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5"
+   type="button"
+  >
+   <span class="sr-only">Open dropdown</span>
+   <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
+    <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
+   </svg>
+  </button>
+ </div>
+ <div class="flex flex-col items-center pb-10">
+  <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="${pokemon['sprites']['front_default']}" alt="Bonnie image" />
+  <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">${pokemon['name']}</h5>
+  <span class="text-sm text-gray-500 dark:text-gray-400">${pokemon['types'][0]['type']['name']}</span>
+  <div class="flex mt-4 space-x-3 md:mt-6">
+   <a
+    href="#"
+    class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+   >
+    Read More
+   </a>
+  </div>
+ </div>
+</div>`;
+ });
+}
+
+getPikachu();
